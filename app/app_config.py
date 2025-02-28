c5eedd58c58934d851379ebf30f22518e02ac3db
bl1nkker <nurovich14@gmail.com>
2025-02-28 16:37:50 +0500

    feat: added app_config file

---
+++ app/app_config.py
@@ -0,0 +1,20 @@
+from typing import Callable
+from utils import generate_get_config_method
+from pydantic import BaseModel
+from typing_extensions import Protocol
+
+
+class AppConfig(BaseModel):
+    canvas_domain: str
+
+
+def _get_config(get_value: Callable[[str], str], is_test: bool) -> dict:
+    return dict(canvas_domain=get_value('CANVAS_DOMAIN'))
+
+
+class GetAppConfigCallable(Protocol):
+    def __call__(self, yaml_file: str = 'app.yaml', **defaults) -> AppConfig:
+        ...
+
+
+get_app_config: GetAppConfigCallable = generate_get_config_method(AppConfig, _get_config)
