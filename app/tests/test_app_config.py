1839d8bec279be55f297575206cf5221534c8efd
bl1nkker <nurovich14@gmail.com>
2025-03-03 10:47:53 +0500

    test: added tests for app_config

---
+++ app/tests/test_app_config.py
@@ -0,0 +1,10 @@
+from app_config import AppConfig, get_app_config
+
+
+class TestAppConfig:
+    def test_get_app_config(self):
+        app_config = get_app_config()
+        assert app_config == AppConfig(
+            canvas_domain="https://canvas.narxoz.kz",
+            encryption_key="Z8qWK_Ce5OOBuISsa2j35UMf6z_a7jkwI3VTAXQj31g=",
+        )
