a260f971644dcf170305b8ffed572270f807587b
bl1nkker <nurovich14@gmail.com>
2025-03-07 14:30:27 +0500

    feat: added entries to docker-entrypoint

--- deployment/local/project/app/docker-entrypoint.sh
+++ deployment/local/project/app/docker-entrypoint.sh
@@ -7,6 +7,10 @@ if [ "$1" == "dbinit" ]; then
     poetry run alembic upgrade head
 elif [ "$1" == 'api' ]; then
     poetry run python ./web/api.py
+elif [ "$1" == "celery-beat" ]; then
+    poetry run celery -A celery_app beat --loglevel=info
+elif [ "$1" == "celery-worker" ]; then
+    poetry run celery -A celery_app worker --loglevel=info
 else
     exec "$@"
 fi
