20a52a5c6489e425c808ec929e13ed54a590c22f
bl1nkker <nurovich14@gmail.com>
2025-03-07 15:22:10 +0500

    ci: added github-actions

---
+++ deployment/ci/py/scripts/common-init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+
+curl -sSL https://install.python-poetry.org | python - --version 1.7.1
+export PATH="/root/.local/bin:$PATH"
+poetry --version
+
+python -m venv .venv
+source .venv/bin/activate
+
+pip install "typer[all]"
+pip install poetry-core==1.4.0
+pip install tomlkit==0.11.8
---
+++ deployment/ci/py/scripts/run-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+trap 'catch $? $LINENO' ERR
+
+catch() {
+  echo "Error $1 occurred on line $2"
+  exit 1
+}
+
+
+set -eux
+python --version
+
+source ./deployment/ci/py/scripts/common-init.sh
+
+pushd app
+
+poetry install --no-root --with tests,linter
+ruff check .
+
+export PYTHON_ENV=test
+poetry run cli dbinit
+[ -d "alembic" ] && alembic upgrade head
+pytest
+
+popd
