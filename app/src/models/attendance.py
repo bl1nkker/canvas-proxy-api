d82b7592459da568e767b76b3c3ff8c4672ad391
bl1nkker <nurovich14@gmail.com>
2025-03-31 12:19:05 +0500

    feat: added web_id to attendance model

--- app/src/models/attendance.py
+++ app/src/models/attendance.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Column, Enum, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 
-from db import DbModel
+from db import DbModel, HasWebId
 from src.enums.attendance_status import AttendanceStatus
 from src.enums.attendance_value import AttendanceValue
 from src.models.base import Base
 
 
-class Attendance(Base, DbModel):
+class Attendance(Base, DbModel, HasWebId):
     __tablename__ = "attendances"
     student_id = Column(Integer, ForeignKey("app.students.id"), nullable=False)
     student = relationship("Student")
