c7f1265461ac67455fdbea0eb5c8e2f4b440326d
bl1nkker <nurovich14@gmail.com>
2025-04-21 14:34:36 +0500

    chore: removed useless load methods

--- app/src/services/student_service.py
+++ app/src/services/student_service.py
@@ -6,7 +6,7 @@ import shortuuid
 
 from db.data_repo import Pagination
 from ml import face_encoder
-from src.dto import auth_dto, enrollment_dto, student_dto
+from src.dto import enrollment_dto, student_dto
 from src.errors.types import InvalidDataError, NotFoundError
 from src.errors.utils import write_to_temp_file
 from src.models.enrollment import Enrollment
@@ -71,41 +71,6 @@ class StudentService:
             )
             self._student_repo.save_or_update(student)
         return student_dto.Read.from_dbmodel(student)
-
-    async def load_students(
-        self, course_web_id: str, canvas_auth_data: auth_dto.CanvasAuthData
-    ) -> list[student_dto.Read]:
-        with self._canvas_course_repo.session():
-            course = self._canvas_course_repo.get_by_web_id(web_id=course_web_id)
-        if not course:
-            raise NotFoundError(message=f"_error_msg_course_not_found:{course_web_id}")
-        course_students = await self._canvas_proxy_provider.get_course_students(
-            canvas_course_id=course.canvas_course_id, cookies=canvas_auth_data
-        )
-        students = []
-        with self._student_repo.session():
-            for student in course_students:
-                student_db = self._student_repo.get_by_canvas_user_id(
-                    canvas_user_id=student.canvas_user_id
-                )
-                if not student_db:
-                    student_db = Student(
-                        web_id=shortuuid.uuid(),
-                        name=student.name,
-                        email=student.email,
-                        canvas_user_id=student.canvas_user_id,
-                    )
-                    self._student_repo.save_or_update(student_db)
-                students.append(student_db)
-
-        for student in students:
-            try:
-                self.enroll_student(web_id=student.web_id, course_web_id=course_web_id)
-            except InvalidDataError:
-                # if student enrollment already exists
-                pass
-
-        return [student_dto.Read.from_dbmodel(student) for student in students]
 
     def save_student_image(
         self,
